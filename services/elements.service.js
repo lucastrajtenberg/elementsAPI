70a1aeb79e35c300cfa347c26f4d01a2eb74f3ff
Lucas <48570788+zaluk365@users.noreply.github.com>
2023-03-27 01:20:40 -0300

    refactor: delete comments

--- services/elements.service.js
+++ services/elements.service.js
@@ -1,13 +1,6 @@
 const elementsModel = require('../db/schemas/elements')
 
 module.exports = {
-    arraysEqual: (array1, array2) => {
-        if(array1.length !== array2.length) return false
-        array1.forEach((element, index) => {
-            if(element !== array2[index]) return false
-        })
-        return true
-    },
     schemaAttributes: async () => {
         return await Object.keys(elementsModel.schema.obj)
     },
